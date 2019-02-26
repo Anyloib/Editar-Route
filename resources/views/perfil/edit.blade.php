8040e8b21283c494bcab39e6cccd6239a757f565
Alumno <ulsa@OscarG21.local>
2019-02-25 18:42:06 -0700

    Editar y Route

---
+++ resources/views/perfil/edit.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.default')
+@section('titulo_pagina','Mascotas | Perfil')
+@section('titulo','Mascotas')
+@section('subtitulo','Perfil')
+@section('contenido')
+
+<div class="row">
+    <div class="col-md-12">
+        <div class="box">
+            <div class="box-header with-dorder">
+                <h3 class="box-title">Perfil</h3>
+            </div>
+            <div class="box-body">
+                Perfil
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
